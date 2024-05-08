cdf96866bf595ccf56d4dec573ab8abc000a3747
FilipS <filips@vaskir.co>
2024-05-08 14:49:11 +0200

    cleanup misc Win-usbreset

--- misc/Win-usbreset.ps1
+++ misc/Win-usbreset.ps1
@@ -1,5 +1,4 @@
 $PnpDevice = $(Get-CimInstance Win32_PnPEntity | where {$_.Name -like 'USB Serial Port*' } | Select-Object -Property InstanceId)
 echo $PnpDevice.InstanceId
-FTDIBUS\VID_0403+PID_6001+6&3365FBAF&0&11\0000
 Disable-PnpDevice -InstanceId $PnpDevice.InstanceId -Confirm:$false
 Enable-PnpDevice -InstanceId $PnpDevice.InstanceId -Confirm:$false
