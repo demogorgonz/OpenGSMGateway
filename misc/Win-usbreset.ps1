d9dda02c33cabd05f188588497053899f3a806ce
FilipS <filips@vaskir.co>
2024-05-16 15:38:46 +0200

    update misc - Win-usbreset.ps1

--- misc/Win-usbreset.ps1
+++ misc/Win-usbreset.ps1
@@ -1,4 +1,4 @@
-$PnpDevice = $(Get-CimInstance Win32_PnPEntity | where {$_.Name -like 'USB Serial Port*' } | Select-Object -Property InstanceId)
-echo $PnpDevice.InstanceId
-Disable-PnpDevice -InstanceId $PnpDevice.InstanceId -Confirm:$false
-Enable-PnpDevice -InstanceId $PnpDevice.InstanceId -Confirm:$false
+$PnpDevice = $(Get-CimInstance Win32_PnPEntity | where {$_.Name -like 'USB Serial Port*' } | Select-Object -Property DeviceID)
+echo $PnpDevice.DeviceID
+Disable-PnpDevice -InstanceId $PnpDevice.DeviceID -Confirm:$false
+Enable-PnpDevice -InstanceId $PnpDevice.DeviceID -Confirm:$false
