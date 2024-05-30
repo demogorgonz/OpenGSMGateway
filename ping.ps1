2519eea4112857e214860f28b5d5fda8d71ab783
FilipS <filips@vaskir.co>
2024-05-30 12:50:41 +0200

    fix ping OS port selection

--- ping.ps1
+++ ping.ps1
@@ -8,9 +8,18 @@ cd $PSScriptRoot
 . "$PSScriptRoot\config.ps1"
 
 if ($PingDevice -eq 'true') {
-
     try {
-        $port = new-Object System.IO.Ports.SerialPort COM$(Get-CimInstance Win32_PnPEntity | where {$_.Name -like 'USB Serial Port*' } |Select-Object Name | select-string \d+ | % { $_.matches.Value }), 115200, None, 8, one
+        if ($IsWindows) {
+            $port = new-Object System.IO.Ports.SerialPort COM$(Get-CimInstance Win32_PnPEntity | where {$_.Name -like 'USB Serial Port*' } |Select-Object Name | select-string \d+ | % { $_.matches.Value }), $BaudRate, None, 8, one
+        }
+        elseif ($IsLinux) {
+            $device = $(dmesg | grep tty | grep -oE '[^ ]+$' | tail -n 1)
+            $port = new-Object System.IO.Ports.SerialPort /dev/$device, $BaudRate, None, 8, one
+        }
+        elseif ($IsMacOS) {
+            Write-Host "macOS"
+            # TODO - needs testing
+        }
         $port.Open()
         start-sleep 5
         if (-not $port.IsOpen) {
